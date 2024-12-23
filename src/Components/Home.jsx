caa2fae1ba7515f15da58493a6bf3d427e1a7881
Rupon <fardiidlamrupon@gmail.com>
2024-12-24 01:32:28 +0600

    add a framer motion on the homepage, scroll progessbar

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,11 +3,35 @@ import ChooseUs from "./ChooseUs";
 import Faq from "./Faq";
 import Features from "./Features";
 import MeetPartners from "./MeetPartners";
+import { motion, useSpring, useScroll } from "motion/react"
 
  
 const Home = () => {
+
+    const { scrollYProgress } = useScroll()
+    const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 30,
+        restDelta: 0.001,
+    })
+
     return (
         <div>
+
+            <motion.div
+                id="scroll-indicator"
+                style={{
+                    scaleX,
+                    position: "fixed",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    height: 6,
+                    originX: 0,
+                    backgroundColor: "#333634",
+                }}
+            />
+
             {/* banenr swiper js */}
             <Banner></Banner>
             {/* featured section */}
